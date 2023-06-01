8a3bfdbb8f28260f00ba42285d90308c9a3da6f9
Tadas <tturo@protonmail.com>
2023-06-01 19:44:15 +0200

    fixed side panel, added purple

--- App/sidepanel.js
+++ App/sidepanel.js
@@ -1,8 +1,19 @@
 // JavaScript code to toggle the side panel
+function toggleSidePanel() {
+  const sidePanel = document.getElementById('side-panel');
+  sidePanel.classList.toggle('show-panel');
+}
+
 const sidePanel = document.getElementById('side-panel');
 const toggleButton = document.getElementById('toggle-button');
 
-toggleButton.addEventListener('click', () => {
-  sidePanel.classList.toggle('show-panel');
-});
+let isPanelOpen = false;
 
+toggleButton.addEventListener('click', () => {
+  isPanelOpen = !isPanelOpen;
+  if (isPanelOpen) {
+    sidePanel.style.transform = 'translateX(0)';
+  } else {
+    sidePanel.style.transform = 'translateX(-100%)';
+  }
+});
