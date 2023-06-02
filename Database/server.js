fb6b785eb0a15eafb59e91ec1a013201b686ebeb
Tadas <tturo@protonmail.com>
2023-06-02 11:10:12 +0200

    fixed server config

--- Database/server.js
+++ Database/server.js
@@ -1,18 +1,32 @@
 const express = require('express');
+const { spawn } = require('child_process');
+
 const app = express();
-const whisper = require('whisper'); // Assuming you have installed the OpenAI Whisper library
 
 app.use(express.json());
 
-app.post('/process-audio', (req, res) => {
-  const audioData = req.body.audio; // Assuming the audio file is sent as a FormData object
+app.post('/summarize', (req, res) => {
+  const text = req.body.text; // Assuming the text is sent in the request body
 
-  // Process the audio using OpenAI Whisper
-  const model = whisper.load_model("base");
-  const result = model.transcribe(audioData);
+  // Spawn a Python process and execute the summarization script
+  const pythonProcess = spawn('python', ['summarization.py', text]);
 
-  // Return the transcription result
-  res.json({ transcription: result.text });
+  let summary = '';
+
+  pythonProcess.stdout.on('data', (data) => {
+    // Capture the output from the Python process
+    summary += data.toString();
+  });
+
+  pythonProcess.on('close', (code) => {
+    if (code === 0) {
+      // Return the summary as the response
+      res.json({ summary });
+    } else {
+      // Handle any errors
+      res.status(500).json({ error: 'An error occurred during summarization' });
+    }
+  });
 });
 
 app.listen(3000, () => {
