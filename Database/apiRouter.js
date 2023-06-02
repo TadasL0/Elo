8a9e5dc1e58110e3e469e84b665638fa31070cb8
Tadas <tturo@protonmail.com>
2023-06-02 16:57:49 +0200

    started config on weekly summary

--- Database/apiRouter.js
+++ Database/apiRouter.js
@@ -1,10 +1,28 @@
 const express = require("express");
-
 const apiRouter = express.Router();
 
 // Define your API routes
 apiRouter.get("/", (req, res) => {
   res.send("API Home");
+});
+
+apiRouter.post("/entries", (req, res) => {
+  // Handle saving the journal entry to the database
+  // Example code:
+  const { title, content } = req.body;
+  // Save the entry to the database using the provided data
+  
+  res.send("Journal entry saved successfully");
+});
+
+apiRouter.post("/summary", (req, res) => {
+  // Handle text summarization
+  // Example code:
+  const { text } = req.body;
+  // Perform the text summarization using the provided text
+  // Return the summary
+  
+  res.send("Summary generated successfully");
 });
 
 // Export the API router
