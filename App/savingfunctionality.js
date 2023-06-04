e7e99cc308c11e1b39b7f12ecc43accd57d99264
Tadas <tturo@protonmail.com>
2023-06-04 18:11:24 +0200

    saving functionality

--- App/savingfunctionality.js
+++ App/savingfunctionality.js
@@ -1,6 +1,6 @@
 const currentDate = new Date().toLocaleDateString();  // Get the current date
 
-const saveEntry = async () => {
+const autosaveEntry = async () => {
   const entry = document.getElementById("journal-entry").value;
   const data = {
     title: `Diary Entry - ${currentDate}`,  // Add the current date to the title
@@ -17,40 +17,13 @@ const saveEntry = async () => {
     });
 
     if (response.ok) {
-      console.log("Journal entry saved successfully");
+      console.log("Journal entry autosaved successfully");
     } else {
-      console.error("Failed to save journal entry:", response.status);
+      console.error("Failed to autosave journal entry:", response.status);
     }
   } catch (error) {
     console.error("An error occurred:", error);
   }
 };
 
-const condenseEntry = async () => {
-  const entry = document.getElementById("journal-entry").value;
-  const data = {
-    text: entry,
-  };
-
-  try {
-    const response = await fetch("http://localhost:3000/api/summary", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (response.ok) {
-      const summary = await response.text();
-      console.log("Summary:", summary);
-    } else {
-      console.error("Failed to retrieve summary:", response.status);
-    }
-  } catch (error) {
-    console.error("An error occurred:", error);
-  }
-};
-
-document.getElementById("save-entry-button").addEventListener("click", saveEntry);
-document.getElementById("condense-button").addEventListener("click", condenseEntry);
+document.addEventListener("DOMContentLoaded", autosaveEntry);
