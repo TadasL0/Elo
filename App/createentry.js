2709ed6cdf4a921f7677d160d40a6d0581ee0bfc
Tadas <tturo@protonmail.com>
2023-06-08 10:29:54 +0300

    side panel fixes

--- App/createentry.js
+++ App/createentry.js
@@ -26,13 +26,11 @@ class Journal {
     this.toggleButton = document.querySelector('.toggle-button');
     this.sidePanel = document.getElementById('side-panel');
     this.newEntryButton = document.querySelector('.new-entry-button');
-    this.entryTitle = document.getElementById('entry-title');
     this.journalEntry = document.getElementById('journal-entry');
 
     // Event listeners
     this.newEntryButton.addEventListener('click', () => this.createNewEntry());
     this.toggleButton.addEventListener('click', () => this.toggleSidePanel());
-    this.entryTitle.addEventListener('click', () => this.editTitle());
     this.journalEntry.addEventListener('input', (event) => this.updateEntryContent(event));
   }
 
@@ -43,9 +41,17 @@ class Journal {
       const entryItem = document.createElement('li');
       entryItem.className = 'entry-item';
       entryItem.textContent = entry.title;
+      entryItem.contentEditable = true; // make the entry title editable
+      entryItem.addEventListener('blur', () => this.renameEntry(index, entryItem.textContent)); // save new name on blur
       entryItem.addEventListener('click', () => this.selectEntry(index));
       this.entryList.appendChild(entryItem);
     });
+  }
+
+  // Function to rename an entry
+  renameEntry(index, newTitle) {
+    this.entries[index].title = newTitle;
+    this.saveEntriesToLocalStorage();
   }
 
   // Function to select an entry
@@ -61,7 +67,6 @@ class Journal {
     });
 
     const selectedEntry = this.entries[this.selectedEntryIndex];
-    this.entryTitle.textContent = selectedEntry.title;
     this.journalEntry.value = selectedEntry.content;
   }
 
@@ -78,26 +83,9 @@ class Journal {
     localStorage.setItem('journalEntries', JSON.stringify(this.entries));
   }
 
-  // Function to edit title
-  editTitle() {
-    const selectedEntry = this.entries[this.selectedEntryIndex];
-
-    // Add editable class to make the title editable
-    this.entryTitle.contentEditable = true;
-    this.entryTitle.classList.add('editable');
-
-    // Save the updated title when the user focuses out of the element
-    this.entryTitle.addEventListener('focusout', () => {
-      selectedEntry.title = this.entryTitle.textContent;
-      this.entryTitle.contentEditable = false;
-      this.entryTitle.classList.remove('editable');
-      this.saveEntriesToLocalStorage();
-    });
-  }
-
   // Function to update entry content
   updateEntryContent(event) {
-    this.entries[this.selectedEntryIndex].content = event.target.value; // Update the content of the selected entry
+    this.entries[this.selectedEntryIndex].content = event.target.value;
     this.saveEntriesToLocalStorage();
   }
 
