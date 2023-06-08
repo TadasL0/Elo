6977e26fc7dce173d22c4443a44612893bc443c0
Tadas <tturo@protonmail.com>
2023-06-08 10:00:39 +0300

    gpt4 sidepanel changes

--- App/createentry.js
+++ App/createentry.js
@@ -1,113 +1,115 @@
-// Retrieve entries from local storage or initialize an empty array
-let entries = JSON.parse(localStorage.getItem("journalEntries")) || [];
+// The entry model
+class Entry {
+  constructor(title = 'New Entry', content = '') {
+    this.title = title;
+    this.content = content;
+  }
+}
 
-// Get the necessary DOM elements
-const entryList = document.getElementById("entry-list");
-const toggleButton = document.querySelector(".toggle-button");
-const sidePanel = document.getElementById("side-panel");
-const newEntryButton = document.querySelector(".new-entry-button");
+// The journal model
+class Journal {
+  constructor() {
+    this.entries = JSON.parse(localStorage.getItem('journalEntries')) || [];
+    this.selectedEntryIndex = null;
+    this.getDomElements();
+    this.renderEntryList();
 
-// Variable to keep track of the selected entry
-let selectedEntryIndex = null;
+    if (this.entries.length > 0) {
+      this.selectEntry(0); // Select the first entry by default
+    } else {
+      this.createNewEntry(); // Create a new entry if the list is empty
+    }
+  }
 
-// Function to render the entry list
-function renderEntryList() {
-  entryList.innerHTML = "";
+  getDomElements() {
+    this.entryList = document.getElementById('entry-list');
+    this.toggleButton = document.querySelector('.toggle-button');
+    this.sidePanel = document.getElementById('side-panel');
+    this.newEntryButton = document.querySelector('.new-entry-button');
+    this.entryTitle = document.getElementById('entry-title');
+    this.journalEntry = document.getElementById('journal-entry');
 
-  entries.forEach((entry, index) => {
-    const entryItem = document.createElement("li");
-    entryItem.className = "entry-item";
-    entryItem.textContent = entry.title;
-    entryItem.addEventListener("click", () => {
-      selectEntry(index);
+    // Event listeners
+    this.newEntryButton.addEventListener('click', () => this.createNewEntry());
+    this.toggleButton.addEventListener('click', () => this.toggleSidePanel());
+    this.entryTitle.addEventListener('click', () => this.editTitle());
+    this.journalEntry.addEventListener('input', (event) => this.updateEntryContent(event));
+  }
+
+  // Function to render the entry list
+  renderEntryList() {
+    this.entryList.innerHTML = '';
+    this.entries.forEach((entry, index) => {
+      const entryItem = document.createElement('li');
+      entryItem.className = 'entry-item';
+      entryItem.textContent = entry.title;
+      entryItem.addEventListener('click', () => this.selectEntry(index));
+      this.entryList.appendChild(entryItem);
+    });
+  }
+
+  // Function to select an entry
+  selectEntry(index) {
+    this.selectedEntryIndex = index;
+    this.entries.forEach((entry, i) => {
+      const entryItem = this.entryList.children[i];
+      if (i === index) {
+        entryItem.classList.add('selected');
+      } else {
+        entryItem.classList.remove('selected');
+      }
     });
 
-    entryList.appendChild(entryItem);
-  });
-}
-
-// Function to select an entry
-function selectEntry(index) {
-  selectedEntryIndex = index;
-
-  entries.forEach((entry, i) => {
-    const entryItem = entryList.children[i];
-    if (i === index) {
-      entryItem.classList.add("selected");
-    } else {
-      entryItem.classList.remove("selected");
-    }
-  });
-
-  const selectedEntry = entries[selectedEntryIndex];
-  document.getElementById("entry-title").textContent = selectedEntry.title;
-  document.getElementById("journal-entry").value = selectedEntry.content;
-}
-
-// Function to create a new entry
-function createNewEntry() {
-  const newEntry = {
-    title: "New Entry",
-    content: "",
-  };
-
-  entries.push(newEntry);
-  saveEntriesToLocalStorage();
-  renderEntryList();
-  selectEntry(entries.length - 1);
-}
-
-// Function to save entries to local storage
-function saveEntriesToLocalStorage() {
-  localStorage.setItem("journalEntries", JSON.stringify(entries));
-}
-
-// Initialize the app
-renderEntryList();
-
-// Check if there are entries in the list
-if (entries.length > 0) {
-  selectEntry(0); // Select the first entry by default
-} else {
-  createNewEntry(); // Create a new entry if the list is empty
-}
-
-// Handle the event when the entry title is clicked
-document.getElementById("entry-title").addEventListener("click", () => {
-  const selectedEntry = entries[selectedEntryIndex];
-  const entryTitle = document.getElementById("entry-title");
-
-  // Add editable class to make the title editable
-  entryTitle.contentEditable = true;
-  entryTitle.classList.add("editable");
-
-  // Save the updated title when the user focuses out of the element
-  entryTitle.addEventListener("focusout", () => {
-    selectedEntry.title = entryTitle.textContent;
-    entryTitle.contentEditable = false;
-    entryTitle.classList.remove("editable");
-    saveEntriesToLocalStorage();
-  });
-});
-
-// Handle the event when the textarea input changes
-document.getElementById("journal-entry").addEventListener("input", (event) => {
-  entries[selectedEntryIndex].content = event.target.value; // Update the content of the selected entry
-  saveEntriesToLocalStorage();
-});
-
-// Toggle the side panel when the toggle button is clicked
-toggleButton.addEventListener("click", () => {
-  sidePanel.classList.toggle("show-panel");
-
-  // Toggle visibility of entry items when side panel is closed
-  const entryItems = document.getElementsByClassName("entry-item");
-  for (let item of entryItems) {
-    item.classList.toggle("entry-visible");
+    const selectedEntry = this.entries[this.selectedEntryIndex];
+    this.entryTitle.textContent = selectedEntry.title;
+    this.journalEntry.value = selectedEntry.content;
   }
-});
 
-// Create new entry when the new entry button is clicked
-newEntryButton.addEventListener("click", () => {
-  createNewEntry();
-});
+  // Function to create a new entry
+  createNewEntry() {
+    this.entries.push(new Entry());
+    this.saveEntriesToLocalStorage();
+    this.renderEntryList();
+    this.selectEntry(this.entries.length - 1);
+  }
+
+  // Function to save entries to local storage
+  saveEntriesToLocalStorage() {
+    localStorage.setItem('journalEntries', JSON.stringify(this.entries));
+  }
+
+  // Function to edit title
+  editTitle() {
+    const selectedEntry = this.entries[this.selectedEntryIndex];
+
+    // Add editable class to make the title editable
+    this.entryTitle.contentEditable = true;
+    this.entryTitle.classList.add('editable');
+
+    // Save the updated title when the user focuses out of the element
+    this.entryTitle.addEventListener('focusout', () => {
+      selectedEntry.title = this.entryTitle.textContent;
+      this.entryTitle.contentEditable = false;
+      this.entryTitle.classList.remove('editable');
+      this.saveEntriesToLocalStorage();
+    });
+  }
+
+  // Function to update entry content
+  updateEntryContent(event) {
+    this.entries[this.selectedEntryIndex].content = event.target.value; // Update the content of the selected entry
+    this.saveEntriesToLocalStorage();
+  }
+
+  // Function to toggle side panel
+  toggleSidePanel() {
+    this.sidePanel.classList.toggle('show-panel');
+    const entryItems = document.getElementsByClassName('entry-item');
+    for (let item of entryItems) {
+      item.classList.toggle('entry-visible');
+    }
+  }
+}
+
+// Start the journal app
+new Journal();
