6674ee3ad5841f72ca745570b7527db519894897
Tadas <tturo@protonmail.com>
2023-06-08 16:19:17 +0300

    loading screen implementatikons

---
+++ App/loadingscreen.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+    var loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.style.display = 'none';
+  }, 2000); // Show loading screen for 2 seconds
+  
+  
+
+
+  
