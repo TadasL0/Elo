6d7a5a22d0b5c5df35b4942d7cd0b7ce6afad17d
Tadas <tturo@protonmail.com>
2023-06-11 12:49:55 +0300

    side panel fixes with dark mode

--- sidepanel.js
+++ sidepanel.js
@@ -1,17 +1,19 @@
-const sidePanel = document.getElementById('side-panel');
-const toggleButton = document.querySelector('.toggle-button');
+window.addEventListener("DOMContentLoaded", (event) => {
+  const sidePanel = document.getElementById('side-panel');
+  const toggleButton = document.querySelector('.toggle-button');
 
-// Set initial style for the side panel
-sidePanel.style.left = "0px";
+  // Set initial style for the side panel
+  sidePanel.style.left = "0px";
 
-// Calculate the width of the side panel
-const sidePanelWidth = sidePanel.offsetWidth;
+  // Calculate the width of the side panel
+  const sidePanelWidth = sidePanel.offsetWidth;
 
-// Adjust the left property when the button is clicked
-toggleButton.addEventListener('click', () => {
-  if (sidePanel.style.left === "0px") {
-    sidePanel.style.left = `-${sidePanelWidth - 63}px`;  // Leaving 50px visible
-  } else {
-    sidePanel.style.left = "0px";
-  }
+  // Adjust the left property when the button is clicked
+  toggleButton.addEventListener('click', () => {
+      if (sidePanel.style.left === "0px") {
+          sidePanel.style.left = `-${sidePanelWidth - 63}px`;  // Leaving 50px visible
+      } else {
+          sidePanel.style.left = "0px";
+      }
+  });
 });
