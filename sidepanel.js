b1bc41c3d80226495e6e315f1c73bcdd0da2651e
Tadas <tturo@protonmail.com>
2023-06-12 21:46:33 +0300

    side panel with modifiers instead of fixed values

--- sidepanel.js
+++ sidepanel.js
@@ -1,19 +1,23 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  const sidePanel = document.getElementById('side-panel');
-  const toggleButton = document.querySelector('.toggle-button');
-
-  // Set initial style for the side panel
-  sidePanel.style.left = "0px";
-
-  // Calculate the width of the side panel
-  const sidePanelWidth = sidePanel.offsetWidth;
-
-  // Adjust the left property when the button is clicked
-  toggleButton.addEventListener('click', () => {
+    const sidePanel = document.getElementById('side-panel');
+    const toggleButton = document.querySelector('.toggle-button');
+  
+    // Set initial style for the side panel
+    sidePanel.style.left = "0px";
+  
+    // Calculate the width of the side panel
+    const sidePanelWidth = sidePanel.offsetWidth;
+  
+    // Calculate the visible portion as a percentage of the window width
+    const visiblePortion = window.innerWidth * 0.09; // This is 10%. Adjust the value as needed.
+  
+    // Adjust the left property when the button is clicked
+    toggleButton.addEventListener('click', () => {
       if (sidePanel.style.left === "0px") {
-          sidePanel.style.left = `-${sidePanelWidth - 56}px`;  // Leaving 50px visible
+        sidePanel.style.left = `-${sidePanelWidth - visiblePortion}px`;  
       } else {
-          sidePanel.style.left = "0px";
+        sidePanel.style.left = "0px";
       }
+    });
   });
-});
+  
