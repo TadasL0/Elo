45784e3e94c4821660de4d4abc08f55cfc743599
Tadas <tturo@protonmail.com>
2023-06-13 11:57:15 +0300

    sidepanel fixes and animation of entries

--- sidepanel.js
+++ sidepanel.js
@@ -1,23 +1,43 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    const sidePanel = document.getElementById('side-panel');
-    const toggleButton = document.querySelector('.toggle-button');
-  
-    // Set initial style for the side panel
-    sidePanel.style.left = "0px";
-  
-    // Calculate the width of the side panel
-    const sidePanelWidth = sidePanel.offsetWidth;
-  
-    // Calculate the visible portion as a percentage of the window width
-    const visiblePortion = window.innerWidth * 0.09; // This is 10%. Adjust the value as needed.
-  
-    // Adjust the left property when the button is clicked
-    toggleButton.addEventListener('click', () => {
-      if (sidePanel.style.left === "0px") {
-        sidePanel.style.left = `-${sidePanelWidth - visiblePortion}px`;  
-      } else {
-        sidePanel.style.left = "0px";
-      }
-    });
+  const sidePanel = document.getElementById('side-panel');
+  const toggleButton = document.querySelector('.toggle-button');
+  const panelEntries = document.querySelectorAll('#side-panel .entry-list');
+
+  // Calculate the width of the side panel
+  const sidePanelWidth = sidePanel.offsetWidth;
+
+  // Calculate the visible portion as a percentage of the window width
+  const visiblePortion = window.innerWidth * 0.06; // This is 10%. Adjust the value as needed.
+
+  // Set initial style for the side panel as closed
+  sidePanel.style.left = `-${sidePanelWidth - visiblePortion}px`;
+
+  // Initially, make the entries invisible
+  panelEntries.forEach(entry => {
+      entry.style.opacity = "0";
   });
-  
+
+  // Adjust the left property when the button is clicked
+  toggleButton.addEventListener('click', () => {
+    if (sidePanel.style.left !== "0px") {
+      sidePanel.style.left = "0px";
+
+      // Make the entries visible after the panel has been opened
+      setTimeout(() => {
+          panelEntries.forEach(entry => {
+              entry.style.opacity = "1";
+          });
+      }, 200); // Delay in milliseconds, adjust as needed
+
+    } else {
+      sidePanel.style.left = `-${sidePanelWidth - visiblePortion}px`;
+      
+      // Delay the entries to disappear after the panel has closed
+      setTimeout(() => {
+          panelEntries.forEach(entry => {
+              entry.style.opacity = "0";
+          });
+      }, 200);  // Delay in milliseconds, adjust as needed
+    }
+  });
+});
