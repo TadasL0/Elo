a88c13d820932a13c307807dc55a2dd4ae39bce5
Tadas <tturo@protonmail.com>
2023-06-13 21:31:15 +0300

    css upgrade to settings

--- settingsfunctionality.js
+++ settingsfunctionality.js
@@ -1,9 +1,21 @@
 function toggleCustomSettingsPanel() {
-    var settingsPanel = document.getElementById('custom-settings-panel');
-    if (settingsPanel.style.transform === 'scaleY(1)') {
-      settingsPanel.style.transform = 'scaleY(0)';
-    } else {
-      settingsPanel.style.transform = 'scaleY(1)';
-    }
-  }
+  var settingsPanel = document.getElementById('custom-settings-panel');
+  var settingsIcon = document.getElementById('custom-settings-icon');
   
+  if (settingsPanel.style.right === '0px') {
+      settingsPanel.style.right = '-260px';
+      settingsIcon.classList.remove('spin');
+  } else {
+      settingsPanel.style.right = '0px';
+      settingsIcon.classList.add('spin');
+  }
+  settingsPanel.classList.toggle('open');
+}
+
+
+// Make sure the DOM is fully loaded before initializing Sortable
+document.addEventListener("DOMContentLoaded", function() {
+  new Sortable(document.getElementById('sortable-question-list'), {
+      animation: 150,
+  });
+});
