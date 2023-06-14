08250637d7239ec064575466008e49f234422e8a
Tadas <tturo@protonmail.com>
2023-06-14 11:09:25 +0300

    settings of questions updated.

--- settingsfunctionality.js
+++ settingsfunctionality.js
@@ -19,3 +19,27 @@ document.addEventListener("DOMContentLoaded", function() {
       animation: 150,
   });
 });
+
+new Sortable(document.getElementById('sortable-question-list'), {
+  animation: 150,
+  store: {
+    /**
+     * Get the order of elements. Called once during initialization.
+     * @param   {Sortable}  sortable
+     * @returns {Array}
+     */
+    get: function (sortable) {
+      var order = localStorage.getItem(sortable.options.group.name);
+      return order ? order.split('|') : [];
+    },
+
+    /**
+     * Save the order of elements. Called onEnd (when the item is dropped).
+     * @param {Sortable}  sortable
+     */
+    set: function (sortable) {
+      var order = sortable.toArray();
+      localStorage.setItem(sortable.options.group.name, order.join('|'));
+    }
+  }
+});
