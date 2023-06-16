5400f816786d88d51072fc7c4b1bd19e38ee129b
Tadas <tturo@protonmail.com>
2023-06-16 17:15:22 +0300

    settings minor re-ordering update

--- questionswap.js
+++ questionswap.js
@@ -1,16 +1,83 @@
-var questions = Array.from(document.getElementById('sortable-question-list').children).map(li => li.textContent);
+// Import the Sortable library
+var Sortable = require('sortablejs');
 
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
+// Reference to the lists
+var fixedList = document.getElementById('sortable-fixed-list');
+var editableList = document.getElementById('sortable-editable-list');
+
+// Function to store lists order to local storage
+function storeOrder() {
+    var fixedOrder = Array.from(fixedList.children).map(li => li.textContent);
+    var editableOrder = Array.from(editableList.children).map(li => li.textContent);
+
+    localStorage.setItem('fixedOrder', JSON.stringify(fixedOrder));
+    localStorage.setItem('editableOrder', JSON.stringify(editableOrder));
+}
+
+// Function to load lists order from local storage
+function loadOrder() {
+    var fixedOrder = JSON.parse(localStorage.getItem('fixedOrder'));
+    var editableOrder = JSON.parse(localStorage.getItem('editableOrder'));
+
+    if (fixedOrder) {
+        // Remove existing items
+        while (fixedList.firstChild) {
+            fixedList.firstChild.remove();
+        }
+
+        // Add items from the stored order
+        fixedOrder.forEach(function (question) {
+            var li = document.createElement('li');
+            li.textContent = question;
+            li.className = 'sortable-item';
+            fixedList.appendChild(li);
+        });
+    }
+
+    if (editableOrder) {
+        // Remove existing items
+        while (editableList.firstChild) {
+            editableList.firstChild.remove();
+        }
+
+        // Add items from the stored order
+        editableOrder.forEach(function (question) {
+            var li = document.createElement('li');
+            li.className = 'sortable-item';
+            li.innerHTML = `<span class="drag-handle"></span> <span contenteditable="true">${question}</span>`;
+            editableList.appendChild(li);
+        });
     }
 }
 
+// Initialize Sortable on the lists
+Sortable.create(fixedList, { animation: 150, onEnd: storeOrder });
+Sortable.create(editableList, { animation: 150, handle: '.drag-handle', onEnd: storeOrder });
+
+// Function to change the question
 function changeQuestion() {
-    shuffleArray(questions);
-    document.getElementById('journal-entry').placeholder = questions[0];
+    var fixedOrder = JSON.parse(localStorage.getItem('fixedOrder')) || [];
+    var editableOrder = JSON.parse(localStorage.getItem('editableOrder')) || [];
+    var questions = fixedOrder.concat(editableOrder);
+    var total = questions.length;
+    var weights = questions.map((_, i) => total - i); // Higher index, higher chance
+    var randomIndex = Math.floor(Math.random() * weights.reduce((a, b) => a + b, 0));
+
+    for (var i = 0; i < weights.length; i++) {
+        if (randomIndex < weights[i]) {
+            document.getElementById('journal-entry').placeholder = questions[i];
+            break;
+        } else {
+            randomIndex -= weights[i];
+        }
+    }
 }
 
-// Call the function when the page loads
-window.onload = changeQuestion;
+// Load the order and change the question when the page loads
+window.onload = function() {
+    loadOrder();
+    changeQuestion();
+};
+
+// Change the question when the 'journal-entry' textarea gets focus
+document.getElementById('journal-entry').addEventListener('focus', changeQuestion);
--- settingsfunctionality.js
+++ settingsfunctionality.js
@@ -3,38 +3,73 @@ function toggleCustomSettingsPanel() {
   var settingsIcon = document.getElementById('custom-settings-icon');
   
   if (settingsPanel.style.right === '0px') {
-      settingsPanel.style.right = '-260px';
-      settingsIcon.classList.remove('spin');
+    settingsPanel.style.right = '-260px';
+    settingsIcon.classList.remove('spin');
   } else {
-      settingsPanel.style.right = '0px';
-      settingsIcon.classList.add('spin');
+    settingsPanel.style.right = '0px';
+    settingsIcon.classList.add('spin');
   }
   settingsPanel.classList.toggle('open');
 }
 
-// Make sure the DOM is fully loaded before initializing Sortable
+// Load list states from localStorage
+function loadListStates() {
+  var fixedList = document.getElementById('sortable-fixed-list');
+  var editableList = document.getElementById('sortable-editable-list');
+  
+  var fixedListState = localStorage.getItem('fixedListState');
+  var editableListState = localStorage.getItem('editableListState');
+  
+  if (fixedListState) {
+    fixedList.innerHTML = fixedListState;
+  }
+  
+  if (editableListState) {
+    editableList.innerHTML = editableListState;
+  }
+}
+
+// Save list states to localStorage
+function saveListStates() {
+  var fixedList = document.getElementById('sortable-fixed-list');
+  var editableList = document.getElementById('sortable-editable-list');
+
+  localStorage.setItem('fixedListState', fixedList.innerHTML);
+  localStorage.setItem('editableListState', editableList.innerHTML);
+}
+
 document.addEventListener("DOMContentLoaded", function() {
+  loadListStates();
+  
   var fixedList = document.getElementById('sortable-fixed-list');
   var editableList = document.getElementById('sortable-editable-list');
 
   Sortable.create(fixedList, {
     group: 'shared', 
     animation: 150,
-    draggable: '.sortable-item', // specifying draggable elements
+    draggable: '.sortable-item',
     onAdd: function (evt) {
       if (fixedList.children.length > 5) {
         editableList.appendChild(fixedList.children[5]);
       }
+      saveListStates();
     },
+    onUpdate: function(evt) {
+      saveListStates();
+    }
   });
 
   Sortable.create(editableList, {
     group: 'shared', 
     animation: 150,
-    draggable: '.sortable-item', // specifying draggable elements
+    draggable: '.sortable-item',
     onAdd: function (evt) {
       var item = editableList.children[evt.newIndex];
       item.setAttribute('contenteditable', 'true');
+      saveListStates();
     },
+    onUpdate: function(evt) {
+      saveListStates();
+    }
   });
 });
