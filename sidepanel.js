1dd91d7a574f46708fb20cd3dcc4e78ac6f65bbe
Tadas <tturo@protonmail.com>
2023-06-16 19:01:00 +0300

    more mobile fixes, disappearing of the settings ic

--- sidepanel.js
+++ sidepanel.js
@@ -2,6 +2,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
   const sidePanel = document.getElementById('side-panel');
   const toggleButton = document.querySelector('.toggle-button');
   const panelEntries = document.querySelectorAll('#side-panel .entry-list');
+  const settingsToggle = document.getElementById('custom-settings-icon'); // <== NEW LINE
 
   // Calculate the width of the side panel
   const sidePanelWidth = sidePanel.offsetWidth;
@@ -24,28 +25,34 @@ window.addEventListener("DOMContentLoaded", (event) => {
       entry.style.opacity = "0";
   });
 
-  // Adjust the left property when the button is clicked
-  toggleButton.addEventListener('click', () => {
-    if (sidePanel.style.left !== "0px") {
-      sidePanel.style.left = "0px";
+// Adjust the left property when the button is clicked
+toggleButton.addEventListener('click', () => {
+  if (sidePanel.style.left !== "0px") {
+    sidePanel.style.left = "0px";
+    settingsToggle.style.opacity = '0';
+    settingsToggle.style.pointerEvents = 'none'; // disable pointer events when opacity is 0
 
-      // Make the entries visible after the panel has been opened
-      setTimeout(() => {
-          panelEntries.forEach(entry => {
-              entry.style.opacity = "1";
-          });
-      }, 200); // Delay in milliseconds, adjust as needed
+    // Make the entries visible after the panel has been opened
+    setTimeout(() => {
+      panelEntries.forEach(entry => {
+        entry.style.opacity = "1";
+      });
+    }, 200); // Delay in milliseconds, adjust as needed
 
-    } else {
-      sidePanel.style.left = `-${sidePanelWidth - visiblePortion}px`;
-      
-      // Delay the entries to disappear after the panel has closed
-      setTimeout(() => {
-          panelEntries.forEach(entry => {
-              entry.style.opacity = "0";
-          });
-      }, 200);  // Delay in milliseconds, adjust as needed
-    }
-  });
+  } else {
+    sidePanel.style.left = `-${sidePanelWidth - visiblePortion}px`;
+    setTimeout(() => {
+      settingsToggle.style.opacity = '1';
+      settingsToggle.style.pointerEvents = 'auto'; // enable pointer events when opacity is 1
+    }, 200); // Delay in milliseconds, adjust as needed
+
+    // Delay the entries to disappear after the panel has closed
+    setTimeout(() => {
+      panelEntries.forEach(entry => {
+        entry.style.opacity = "0";
+      });
+    }, 200);  // Delay in milliseconds, adjust as needed
+  }
+});
 });
 
