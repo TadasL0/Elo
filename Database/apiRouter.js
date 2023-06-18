4ed6600fbc2a884d9a897428c15795668a09187a
Tadas <tturo@protonmail.com>
2023-06-18 13:49:46 +0300

    server routing update

--- Database/apiRouter.js
+++ Database/apiRouter.js
@@ -9,44 +9,39 @@ module.exports = (pool) => {
   });
 
   // GET route for /api/entries
-  apiRouter.get("/entries", (req, res) => {
+  apiRouter.get("/entries", async (req, res) => {
     console.log("Received a GET request to /api/entries");
 
-    // Retrieve the journal entries from the PostgreSQL database
-    pool.query(
-      "SELECT * FROM entries",
-      [],
-      (err, result) => {
-        if (err) {
-          console.error("Error retrieving journal entries:", err);
-          res.status(500).json({ error: "An error occurred while retrieving the journal entries" });
-        } else {
-          console.log("Journal entries retrieved successfully");
-          res.status(200).json({ message: "Journal entries retrieved successfully", data: result.rows });
-        }
-      }
-    );
+    try {
+      // Retrieve the journal entries from the PostgreSQL database
+      const result = await pool.query("SELECT * FROM entries");
+      console.log("Journal entries retrieved successfully");
+      res.status(200).json({ message: "Journal entries retrieved successfully", data: result.rows });
+    } catch (err) {
+      console.error("Error retrieving journal entries:", err);
+      res.status(500).json({ error: "An error occurred while retrieving the journal entries" });
+    }
   });
 
   // POST route for /api/entries
-  apiRouter.post("/entries", (req, res) => {
+  apiRouter.post("/entries", express.json(), async (req, res) => {
     console.log("Received a POST request to /api/entries");
-    const { title, content } = req.body;
 
-    // Save the journal entry to the PostgreSQL database
-    pool.query(
-      "INSERT INTO entries (title, content) VALUES ($1, $2)",
-      [title, content],
-      (err, result) => {
-        if (err) {
-          console.error("Error saving journal entry:", err);
-          res.status(500).json({ error: "An error occurred while saving the journal entry" });
-        } else {
-          console.log("Journal entry saved successfully");
-          res.status(200).json({ message: "Journal entry saved successfully" });
-        }
-      }
-    );
+    try {
+      const { title, content } = req.body;
+
+      // Save the journal entry to the PostgreSQL database
+      await pool.query(
+        "INSERT INTO entries (title, content) VALUES ($1, $2)",
+        [title, content]
+      );
+      
+      console.log("Journal entry saved successfully");
+      res.status(200).json({ message: "Journal entry saved successfully" });
+    } catch (err) {
+      console.error("Error saving journal entry:", err);
+      res.status(500).json({ error: "An error occurred while saving the journal entry" });
+    }
   });
 
   return apiRouter;
