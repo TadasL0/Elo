fb250b3bd9d71a2b1a50b78676dbb0690f58d19c
Tadas <tturo@protonmail.com>
2023-06-18 14:56:08 +0300

    saving config updates

--- saving.js
+++ saving.js
@@ -1,40 +1,41 @@
 let autosaveTimer;
+const ENTRY_MAX_LENGTH = 5000;
+const AUTO_SAVE_DELAY = 1000; // in ms
+const API_ENDPOINT = "https://app.eloskill.com/api/entries";
+const LOCAL_STORAGE_KEY = "journalEntry";
 
 const autosaveEntry = () => {
   if (autosaveTimer) clearTimeout(autosaveTimer);
 
   autosaveTimer = setTimeout(() => {
-    const entry = document.getElementById("journal-entry").value;
-
-    if (entry.trim() !== "" && entry.length <= 5000) {  // Check if entry is not empty and not too long
-      const data = buildEntryData(entry);
-      saveEntry(data);
-    }
-
     try {
-      saveToLocalStore("journalEntry", entry);
+      const entry = document.getElementById("journal-entry").value;
+
+      if (entry.trim() !== "" && entry.length <= ENTRY_MAX_LENGTH) {
+        const data = buildEntryData(entry);
+        saveEntry(data);
+      }
+
+      saveToLocalStore(LOCAL_STORAGE_KEY, entry);
     } catch (error) {
-      console.error("An error occurred while saving to Local Storage:", error);
+      console.error("An error occurred during autosave:", error);
     }
-  }, 1000);
+  }, AUTO_SAVE_DELAY);
 };
 
-// Function to build the entry data
 const buildEntryData = (entry) => {
-  const currentDate = new Date(); // Get the current date
-  const data = {
+  const currentDate = new Date();
+  
+  return {
     title: `Diary Entry - ${currentDate.toLocaleDateString()}`,
     content: entry,
-    date: currentDate.toISOString(), // convert date to string in ISO format
+    date: currentDate.toISOString(),
   };
-
-  return data;
 };
-
 
 const saveEntry = async (data) => {
   try {
-    const response = await fetch("https://app.eloskill.com/api/entries", {
+    const response = await fetch(API_ENDPOINT, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -57,7 +58,7 @@ const saveToLocalStore = (key, value) => {
   try {
     localStorage.setItem(key, value);
   } catch (error) {
-    throw new Error(`Failed to save to Local Storage: ${error}`);
+    console.error(`Failed to save to Local Storage: ${error}`);
   }
 };
 
@@ -65,22 +66,23 @@ const loadFromLocalStore = (key) => {
   try {
     return localStorage.getItem(key);
   } catch (error) {
-    throw new Error(`Failed to load from Local Storage: ${error}`);
+    console.error(`Failed to load from Local Storage: ${error}`);
   }
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  const journalEntryElement = document.getElementById("journal-entry");
-  journalEntryElement.addEventListener("input", autosaveEntry);
-  
+const setupPage = () => {
   try {
-    const savedEntry = loadFromLocalStore("journalEntry");
+    const journalEntryElement = document.getElementById("journal-entry");
+    journalEntryElement.addEventListener("input", autosaveEntry);
+    
+    const savedEntry = loadFromLocalStore(LOCAL_STORAGE_KEY);
     if (savedEntry) {
       journalEntryElement.value = savedEntry;
     }
   } catch (error) {
-    console.error("An error occurred while loading from Local Storage:", error);
+    console.error("An error occurred while setting up the page:", error);
   }
-});
+};
 
-
+// We use window.onload here instead of DOMContentLoaded to make sure that all external resources are loaded.
+window.onload = setupPage;
