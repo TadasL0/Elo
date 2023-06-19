797bd33e75c3da8f7475b3f73dcf3ca3cf21e0ab
Tadas <tturo@protonmail.com>
2023-06-19 16:59:12 +0300

    streaks

--- streak.js
+++ streak.js
@@ -1,31 +1,33 @@
-function updateStreak() {
-    const streakCountElement = document.getElementById('streak-count');
-  
-    // Get today's date, but only up to the day, not including time
-    let today = new Date().toISOString().slice(0, 10);
-  
-    let lastVisit = localStorage.getItem('lastVisit');
-    let streakCount = localStorage.getItem('streakCount');
-  
-    if (lastVisit === null) {
-      // If no record of last visit, this is the first visit
-      streakCount = 1;
-    } else if (lastVisit !== today) {
-      // If last visit is not today, increment the streak
-      streakCount = Number(streakCount) + 1;
-    }
-  
-    // Update last visit and streak count in local storage
-    localStorage.setItem('lastVisit', today);
-    localStorage.setItem('streakCount', streakCount);
-  
-    // Update streak count on the page
+function updateStreak(storage = window.localStorage) {
+  const streakCountElement = document.getElementById('streak-count');
+
+  // Get today's date, but only up to the day, not including time
+  let today = new Date().toISOString().slice(0, 10);
+
+  let lastVisit = storage.getItem('lastVisit');
+  let streakCount = storage.getItem('streakCount');
+
+  if (lastVisit === null) {
+    // If no record of last visit, this is the first visit
+    streakCount = 1;
+  } else if (lastVisit !== today) {
+    // If last visit is not today, increment the streak
+    streakCount = Number(streakCount) + 1;
+  }
+
+  // Update last visit and streak count in local storage
+  storage.setItem('lastVisit', today);
+  storage.setItem('streakCount', streakCount);
+
+  // Update streak count on the page
+  if (streakCountElement) {
     streakCountElement.textContent = streakCount;
   }
-  
-  window.onload = function() {
-    // Update the streak as soon as the page loads
-    updateStreak();
-  };
-  
+}
+
+window.onload = function() {
+  // Update the streak as soon as the page loads
+  updateStreak();
+};
+
 module.exports = { updateStreak };
