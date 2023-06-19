c9749d72ee2d8d4ae6e4fae60e0f8ec71e0eda4d
Tadas <tturo@protonmail.com>
2023-06-19 17:06:22 +0300

    streaks test update

--- __tests__/streak.test.js
+++ __tests__/streak.test.js
@@ -1,32 +1,15 @@
 const { updateStreak } = require('../streak.js');
 
 describe('updateStreak', () => {
-  // Mock the localStorage object
-  const localStorageMock = {
-    getItem: jest.fn(),
-    setItem: jest.fn(),
-  };
-  
-  // Save the original localStorage
-  const originalLocalStorage = global.localStorage;
-
-  // Set up the DOM
-  document.body.innerHTML = '<div id="streak-count">1</div>';
-
-  // Use the mock localStorage for each test
-  beforeEach(() => {
-    global.localStorage = localStorageMock;
-  });
-
-  // Restore the original localStorage after each test
-  afterEach(() => {
-    global.localStorage = originalLocalStorage;
-  });
-
   it('increments the streak count by 1 if the last visit was not today', () => {
-    localStorageMock.getItem.mockReturnValue('2023-05-01');
-    
-    updateStreak();
+    const localStorageMock = {
+      getItem: jest.fn(() => '2023-05-01'),
+      setItem: jest.fn(),
+    };
+
+    document.body.innerHTML = '<div id="streak-count">1</div>';
+
+    updateStreak(localStorageMock);
     
     expect(localStorageMock.setItem).toHaveBeenCalledWith('streakCount', '2');
   });
