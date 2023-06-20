38b586b287f152251dddb5e268d2c6a0d9c410dd
Tadas <tturo@protonmail.com>
2023-06-20 15:16:17 +0300

    sidepanel merge

--- sidepanel.js
+++ sidepanel.js
@@ -1,21 +1,32 @@
 console.log('Running sidepanel script');
 
+// The entry model
+class Entry {
+  constructor(id = `entry-${Date.now()}`, text = 'Click to change this prompt.') {
+    this.id = id;
+    this.text = text;
+  }
+}
+
 window.addEventListener("DOMContentLoaded", (event) => {
   const sidePanel = document.getElementById('side-panel');
   const sidePanelButton = document.getElementById('side-panel-button');
   const newEntryButton = document.querySelector('.new-entry-button');
-  
-  const entryList = document.getElementById('sortable-editable-list');
   const deleteArea = document.getElementById('delete-area');
-
-  loadEntriesFromLocalStorage();
+  
+  let entries = loadEntriesFromLocalStorage();
 
   // Event listener for new entry button
-  newEntryButton.addEventListener('click', createNewEntry);
+  newEntryButton.addEventListener('click', () => {
+    const newEntry = new Entry();
+    entries.push(newEntry);
+    createNewEntry(newEntry);
+    saveEntriesToLocalStorage();
+  });
 
   // Adjust the left property when the button is clicked
   sidePanelButton.addEventListener('click', () => {
-    const panelEntries = document.querySelectorAll('#entry-list li');
+    const panelEntries = document.querySelectorAll('#sortable-editable-list .sortable-item');
     if (sidePanel.style.left === "0px") {
       sidePanel.style.left = "-250px";
       setTimeout(() => {
@@ -23,6 +34,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
           entry.style.opacity = "0";
         });
         newEntryButton.style.opacity = "0";
+        deleteArea.style.opacity = "0";
       }, 200); 
     } else {
       sidePanel.style.left = "0px";
@@ -31,6 +43,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
           entry.style.opacity = "1";
         });
         newEntryButton.style.opacity = "1";
+        deleteArea.style.opacity = "1";
       }, 200);
     }
   });
@@ -42,7 +55,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const elementToRemove = document.getElementById(id);
     if (elementToRemove) {
       elementToRemove.remove();
-      // Save entries to local storage whenever an entry is deleted
+      entries = entries.filter(entry => entry.id !== id);
       saveEntriesToLocalStorage();
     }
   });
@@ -53,89 +66,44 @@ window.addEventListener("DOMContentLoaded", (event) => {
   });
 
   // Initiate sortable
-  Sortable.create(entryList, {
+  Sortable.create(sortableEditableList, {
     animation: 150,
     onEnd: saveEntriesToLocalStorage
   });
 });
 
 // Function to create a new entry when the plus button is clicked
-function createNewEntry() {
+function createNewEntry(entry) {
   const entryList = document.getElementById('sortable-editable-list');
-  const newEntryId = `entry-${Date.now()}`;
-
-  const newEntry = document.createElement('li');
-  newEntry.classList.add('sortable-item');
-  newEntry.setAttribute('draggable', 'true');
-  newEntry.setAttribute('id', newEntryId);
-  
-  const newEntryText = document.createElement('span');
-  newEntryText.classList.add('editable-text');
-  newEntryText.setAttribute('contenteditable', 'true');
-  newEntryText.innerText = 'New entry';
-
-  newEntry.appendChild(newEntryText);
-  entryList.appendChild(newEntry);
-
-  // Event listener for editing
-  newEntryText.addEventListener('dblclick', startEditing);
-  newEntryText.addEventListener('blur', stopEditing);
-  newEntryText.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      stopEditing(event);
-    }
-  });
-  
-  // Save entries to local storage whenever a new entry is created
-  saveEntriesToLocalStorage();
+  const newEntryHTML = `
+    <li id="${entry.id}" draggable="true" class="sortable-item" ondragstart="drag(event)">
+      <span class="drag-handle"></span> <span contenteditable="true">${entry.text}</span>
+    </li>
+  `;
+  entryList.insertAdjacentHTML('beforeend', newEntryHTML);
 }
 
-function startEditing(event) {
-  event.target.setAttribute('contenteditable', 'true');
+// Function for the drag event
+function drag(event) {
+  event.dataTransfer.setData('text/plain', event.target.id);
 }
 
-function stopEditing(event) {
-  event.target.setAttribute('contenteditable', 'false');
-  saveEntriesToLocalStorage();
-}
-
-function saveEntriesToLocalStorage() {
-  const entryList = document.getElementById('sortable-editable-list');
-  const entries = Array.from(entryList.querySelectorAll('li'));
-  const entriesData = entries.map((entry, index) => {
-    return { id: entry.id, innerText: entry.innerText, order: index };
-  });
-  localStorage.setItem('entries', JSON.stringify(entriesData));
-}
-
+// Function to load entries from local storage
 function loadEntriesFromLocalStorage() {
-  const entriesData = JSON.parse(localStorage.getItem('entries'));
-  if (entriesData) {
-    const entryList = document.getElementById('sortable-editable-list');
-    entriesData.sort((a, b) => a.order - b.order).forEach(entryData => {
-      const newEntry = document.createElement('li');
-      newEntry.classList.add('sortable-item');
-      newEntry.setAttribute('draggable', 'true');
-      newEntry.setAttribute('id', entryData.id);
-      
-      const newEntryText = document.createElement('span');
-      newEntryText.classList.add('editable-text');
-      newEntryText.setAttribute('contenteditable', 'true');
-      newEntryText.innerText = entryData.innerText;
+  const savedEntries = JSON.parse(localStorage.getItem('entries') || "[]");
+  const entryList = document.getElementById('sortable-editable-list');
+  entryList.innerHTML = '';
+  savedEntries.forEach(entry => {
+    createNewEntry(entry);
+  });
+  return savedEntries;
+}
 
-      newEntry.appendChild(newEntryText);
-      entryList.appendChild(newEntry);
-
-      // Event listener for editing
-      newEntryText.addEventListener('dblclick', startEditing);
-      newEntryText.addEventListener('blur', stopEditing);
-      newEntryText.addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-          event.preventDefault();
-          stopEditing(event);
-        }
-      });
-    });
-  }
+// Function to save entries to local storage
+function saveEntriesToLocalStorage() {
+  const entryListItems = document.querySelectorAll('#sortable-editable-list .sortable-item');
+  const entriesToSave = Array.from(entryListItems).map(entryListItem => {
+    return new Entry(entryListItem.id, entryListItem.querySelector('span[contenteditable]').innerText);
+  });
+  localStorage.setItem('entries', JSON.stringify(entriesToSave));
 }
