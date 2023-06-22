1a0fbad621b7f9158e0ee6ec719830377adb43d2
Tadas <tturo@protonmail.com>
2023-06-22 12:10:37 +0300

    entries update

--- entries.js
+++ entries.js
@@ -1,57 +1,30 @@
-console.log('Running entries script');
-
 class Entry {
-  constructor(id = `entry-${Date.now()}`, text = 'Click to change this prompt.') {
+  constructor(id = `entry-${Date.now()}`, text = 'Click to change this prompt.', content = '') {
     this.id = id;
     this.text = text;
+    this.content = content;
   }
 }
 
-window.addEventListener("DOMContentLoaded", (event) => {
-  const entriesPanel = document.getElementById('entries-panel');
-  const entriesPanelButton = document.getElementById('entries-panel-button');
-  const newEntryButton = document.querySelector('.new-entry-button');
-  const entriesDeleteArea = document.getElementById('entries-delete-area');
-  const entriesList = document.getElementById('entries-list');  // Add this line
+let entries = [];
+let currentEntryId = null;
 
-  let entries = loadEntriesFromLocalStorage();
+window.onload = function() {
+  entries = loadEntriesFromLocalStorage();
 
-  Sortable.create(entriesList, {
-    animation: 150,
-    onEnd: () => {
-      const entryOrderArray = Array.from(entriesList.children).map(li => li.id);
-      localStorage.setItem('entryOrder', JSON.stringify(entryOrderArray));
-      saveEntriesToLocalStorage();
-    }
-  });
+  const addEntryButton = document.getElementById('add-entry-button');
+  addEntryButton.addEventListener('click', addNewEntry);
 
-  newEntryButton.addEventListener('click', () => {
-    const newEntry = new Entry();
-    entries.push(newEntry);
-    console.log(entries);  // Add this line
-    createNewEntry(newEntry);
-    saveEntriesToLocalStorage();
-  });  
+  const textarea = document.getElementById('journal-entry');
+  textarea.addEventListener('input', autosaveEntry);
+};
 
-  entriesPanelButton.addEventListener('click', () => {
-    if(entriesPanel) {  // Add this line
-      toggleEntriesPanel();
-    }
-  });
-
-  entriesDeleteArea.addEventListener('drop', (event) => {
-    const id = event.dataTransfer.getData('text/plain');
-    if (!id) return;  // Add this line
-    removeEntryById(id);
-  });
-
-  entriesDeleteArea.addEventListener('dragover', (event) => {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = 'move';
-  });
-
-  setInterval(updatePlaceholderText, 5000);
-});
+function addNewEntry() {
+  const entry = new Entry();
+  entries.push(entry);
+  createNewEntry(entry);
+  saveEntriesToLocalStorage();
+}
 
 function createNewEntry(entry) {
   const entryList = document.getElementById('entries-list');
@@ -59,54 +32,32 @@ function createNewEntry(entry) {
     <li id="${entry.id}" draggable="true" class="entry-item" ondragstart="drag(event)" data-id="${entry.id}">
       <div>
         <span class="drag-handle"></span>
-        <span contenteditable="true" oninput="saveEntriesToLocalStorage()">${entry.text}</span>
+        <span contenteditable="true">${entry.text}</span>
       </div>
     </li>
   `;
   entryList.insertAdjacentHTML('beforeend', newEntryHTML);
+  const newEntryElement = document.getElementById(entry.id);
+  newEntryElement.addEventListener('click', () => {
+    const textarea = document.getElementById('journal-entry');
+    textarea.value = entry.content;
+    currentEntryId = entry.id;
+  });
 }
 
-function drag(event) {
-  let target = event.target;
-  while (target && target.nodeName !== "LI") {
-    target = target.parentNode;
+function autosaveEntry() {
+  const textarea = document.getElementById('journal-entry');
+  const currentEntry = entries.find(entry => entry.id === currentEntryId);
+  if (currentEntry) {
+    currentEntry.content = textarea.value;
+    saveEntriesToLocalStorage();
   }
-  if (!target) return;
-  event.stopPropagation();
-  event.dataTransfer.setData('text/plain', target.id);
 }
 
 function loadEntriesFromLocalStorage() {
-  let savedEntries;
-  try {
-    savedEntries = JSON.parse(localStorage.getItem('entries') || "[]");
-  } catch(e) {
-    console.error("Error parsing entries from local storage:", e);
-    savedEntries = [];
-  }
-  
-  let entryOrder;
-  try {
-    entryOrder = JSON.parse(localStorage.getItem('entryOrder') || "[]");
-  } catch(e) {
-    console.error("Error parsing entry order from local storage:", e);
-    entryOrder = [];
-  }
-
-  savedEntries.sort((a, b) => {
-    const indexA = entryOrder.indexOf(a.id);
-    const indexB = entryOrder.indexOf(b.id);
-    if (indexA === -1 || indexB === -1) {
-      return 0;
-    } else {
-      return indexA - indexB;
-    }
-  });
-
-  const entryList = document.getElementById('entries-list');
-  entryList.innerHTML = '';
-  savedEntries.forEach(entry => {
-    createNewEntry(entry);
+  const savedEntries = JSON.parse(localStorage.getItem('entries') || "[]");
+  savedEntries.forEach(savedEntry => {
+    createNewEntry(new Entry(savedEntry.id, savedEntry.text, savedEntry.content));
   });
   return savedEntries;
 }
@@ -114,50 +65,35 @@ function loadEntriesFromLocalStorage() {
 function saveEntriesToLocalStorage() {
   const entryListItems = document.querySelectorAll('#entries-list .entry-item');
   const entriesToSave = Array.from(entryListItems).map(entryListItem => {
-    return new Entry(entryListItem.id, entryListItem.querySelector('span[contenteditable]').innerText);
+    const entryId = entryListItem.id;
+    const entryText = entryListItem.querySelector('span[contenteditable]').innerText;
+    const entryContent = entries.find(entry => entry.id === entryId)?.content;
+    return new Entry(entryId, entryText, entryContent);
   });
   localStorage.setItem('entries', JSON.stringify(entriesToSave));
 }
 
-function removeEntryById(id) {
-  const elementToRemove = document.getElementById(id);
-  if (elementToRemove) {
-    elementToRemove.remove();
-    saveEntriesToLocalStorage();
-  }
+function removeEntry(id) {
+  entries = entries.filter(entry => entry.id !== id);
+  saveEntriesToLocalStorage();
 }
 
-function updatePlaceholderText() {
-  const textarea = document.querySelector("#journal-entry");
-  if (entries?.length > 0) {
-    const randomIndex = Math.floor(Math.random() * entries.length);
-    const randomEntry = entries[randomIndex];
-    textarea.placeholder = randomEntry.text;
-  }
+// Implement the drag and drop functions
+function drag(event) {
+  const entryItem = event.target.closest('.entry-item');
+  event.dataTransfer.setData('text', entryItem.id);
+  // Temporarily disable contentEditable during drag operation
+  entryItem.querySelector('span[contenteditable]').contentEditable = false;
 }
 
-const faBookIcon = document.getElementById('fa-book-icon');
-const entriesPanel = document.getElementById('entries-panel');  // Move this line outside of the event listener
-
-faBookIcon.addEventListener('click', () => {
-  if(entriesPanel) {
-    toggleEntriesPanel();
-  }
-});
-
-
-function toggleEntriesPanel() {
-  const entriesPanel = document.getElementById('entries-panel');
-  const panelEntries = document.querySelectorAll('#entries-list .entry-item');
-  const newEntryButton = document.querySelector('.new-entry-button');
-
-  const isPanelOpen = getComputedStyle(entriesPanel).left === "0px";
-  
-  entriesPanel.style.left = isPanelOpen ? "-250px" : "0px";
-
-  panelEntries.forEach(entry => {
-    entry.style.opacity = isPanelOpen ? "1" : "1";
-  });
-  newEntryButton.style.opacity = isPanelOpen ? "1" : "1";
+function allowDrop(event) {
+  event.preventDefault();
 }
 
+function drop(event) {
+  event.preventDefault();
+  const data = event.dataTransfer.getData('text');
+  event.target.appendChild(document.getElementById(data));
+  // Re-enable contentEditable after drop operation
+  document.getElementById(data).querySelector('span[contenteditable]').contentEditable = true;
+}
