1a3382f978e39c576dff26613b0cf74f4ac5ed4d
Tadas <tturo@protonmail.com>
2023-06-22 11:25:30 +0300

    broke opening tab but saving entries names

--- entries.js
+++ entries.js
@@ -15,6 +15,15 @@ window.addEventListener("DOMContentLoaded", (event) => {
   
   let entries = loadEntriesFromLocalStorage();
 
+  Sortable.create(entriesList, {
+    animation: 150,
+    onEnd: () => {
+      const entryOrderArray = Array.from(entriesList.children).map(li => li.id);
+      localStorage.setItem('entryOrder', JSON.stringify(entryOrderArray));
+      saveEntriesToLocalStorage();
+    }
+  });
+
   newEntryButton.addEventListener('click', () => {
     const newEntry = new Entry();
     entries.push(newEntry);
@@ -23,35 +32,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
   });
 
   entriesPanelButton.addEventListener('click', () => {
-    const panelEntries = document.querySelectorAll('#entries-list .entry-item');
-    if (entriesPanel.style.left === "0px") {
-      entriesPanel.style.left = "-250px";
-      setTimeout(() => {
-        panelEntries.forEach(entry => {
-          entry.style.opacity = "0";
-        });
-        newEntryButton.style.opacity = "0";
-      }, 200); 
-    } else {
-      entriesPanel.style.left = "0px";
-      setTimeout(() => {
-        panelEntries.forEach(entry => {
-          entry.style.opacity = "1";
-        });
-        newEntryButton.style.opacity = "1";
-      }, 200);
-    }
+    toggleEntriesPanel();
   });
 
   entriesDeleteArea.addEventListener('drop', (event) => {
-    event.preventDefault();
     const id = event.dataTransfer.getData('text/plain');
-    const elementToRemove = document.getElementById(id);
-    if (elementToRemove) {
-      elementToRemove.remove();
-      entries = entries.filter(entry => entry.id !== id);
-      saveEntriesToLocalStorage();
-    }
+    removeEntryById(id);
   });
 
   entriesDeleteArea.addEventListener('dragover', (event) => {
@@ -59,17 +45,17 @@ window.addEventListener("DOMContentLoaded", (event) => {
     event.dataTransfer.dropEffect = 'move';
   });
 
-  Sortable.create(document.getElementById('entries-list'), {
-    animation: 150,
-    onEnd: saveEntriesToLocalStorage
-  });
+  setInterval(updatePlaceholderText, 5000);
 });
 
 function createNewEntry(entry) {
   const entryList = document.getElementById('entries-list');
   const newEntryHTML = `
-    <li id="${entry.id}" draggable="true" class="entry-item" ondragstart="drag(event)">
-      <span class="drag-handle"></span> <span contenteditable="true">${entry.text}</span>
+    <li id="${entry.id}" draggable="true" class="entry-item" ondragstart="drag(event)" data-id="${entry.id}">
+      <div>
+        <span class="drag-handle"></span>
+        <span contenteditable="true">${entry.text}</span>
+      </div>
     </li>
   `;
   entryList.insertAdjacentHTML('beforeend', newEntryHTML);
@@ -87,6 +73,18 @@ function drag(event) {
 
 function loadEntriesFromLocalStorage() {
   const savedEntries = JSON.parse(localStorage.getItem('entries') || "[]");
+  const entryOrder = JSON.parse(localStorage.getItem('entryOrder') || []);
+  
+  savedEntries.sort((a, b) => {
+    const indexA = entryOrder.indexOf(a.id);
+    const indexB = entryOrder.indexOf(b.id);
+    if (indexA === -1 || indexB === -1) {
+      return 0;
+    } else {
+      return indexA - indexB;
+    }
+  });
+
   const entryList = document.getElementById('entries-list');
   entryList.innerHTML = '';
   savedEntries.forEach(entry => {
@@ -103,15 +101,36 @@ function saveEntriesToLocalStorage() {
   localStorage.setItem('entries', JSON.stringify(entriesToSave));
 }
 
-// Function to update placeholder text
+function removeEntryById(id) {
+  const elementToRemove = document.getElementById(id);
+  if (elementToRemove) {
+    elementToRemove.remove();
+    entries = entries.filter(entry => entry.id !== id);
+    saveEntriesToLocalStorage();
+  }
+}
+
 function updatePlaceholderText() {
   const textarea = document.querySelector("#journal-entry");
-  if (entries.length > 0) {
+  if (entries?.length > 0) {
     const randomIndex = Math.floor(Math.random() * entries.length);
     const randomEntry = entries[randomIndex];
     textarea.placeholder = randomEntry.text;
   }
 }
 
-// Set an interval to update placeholder text every 5 seconds
-setInterval(updatePlaceholderText, 5000);
+function toggleEntriesPanel() {
+  const entriesPanel = document.getElementById('entries-panel');
+  const panelEntries = document.querySelectorAll('#entries-list .entry-item');
+  const newEntryButton = document.querySelector('.new-entry-button');
+  
+  const isPanelOpen = entriesPanel.style.left === "0px";
+
+  entriesPanel.style.left = isPanelOpen ? "-300px" : "0px";
+  setTimeout(() => {
+    panelEntries.forEach(entry => {
+      entry.style.opacity = isPanelOpen ? "0" : "1";
+    });
+    newEntryButton.style.opacity = isPanelOpen ? "0" : "1";
+  }, 200);
+}
