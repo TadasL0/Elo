71ca0072256cc4206b299120f84f57765b22af45
Tadas <tturo@protonmail.com>
2023-06-28 15:12:20 +0300

    new feedback loop functionality start

--- Reflection/saving.js
+++ Reflection/saving.js
@@ -1,19 +1,21 @@
 let autosaveTimer;
+let entryModified = false;
 const ENTRY_MAX_LENGTH = 5000;
-const AUTO_SAVE_DELAY = 5000; // in ms, 5s
+const AUTO_SAVE_DELAY = 15000; // in ms, 15s
 const API_ENDPOINT = "https://app.eloskill.com/api/entries";
 const LOCAL_STORAGE_KEY = "journalEntry";
 
 const autosaveEntry = () => {
   if (autosaveTimer) clearTimeout(autosaveTimer);
 
-  autosaveTimer = setTimeout(() => {
+  autosaveTimer = setTimeout(async () => {
     try {
       const entry = document.getElementById("journal-entry").value;
 
-      if (entry.trim() !== "" && entry.length <= ENTRY_MAX_LENGTH) {
+      if (entryModified && entry.trim() !== "" && entry.length <= ENTRY_MAX_LENGTH) {
         const data = buildEntryData(entry);
-        saveEntry(data);
+        await saveEntry(data);
+        entryModified = false;
       }
 
       saveToLocalStore(LOCAL_STORAGE_KEY, entry);
@@ -23,15 +25,11 @@ const autosaveEntry = () => {
   }, AUTO_SAVE_DELAY);
 };
 
-const buildEntryData = (entry) => {
-  const currentDate = new Date();
-  
-  return {
-    title: `Diary Entry - ${currentDate.toLocaleDateString()}`,
-    content: entry,
-    date: currentDate.toISOString(),
-  };
-};
+const buildEntryData = (entry) => ({
+  title: `Diary Entry - ${new Date().toLocaleDateString()}`,
+  content: entry,
+  date: new Date().toISOString(),
+});
 
 const saveEntry = async (data) => {
   try {
@@ -73,17 +71,19 @@ const loadFromLocalStore = (key) => {
 const setupPage = () => {
   try {
     const journalEntryElement = document.getElementById("journal-entry");
-    
     const savedEntry = loadFromLocalStore(LOCAL_STORAGE_KEY);
+
     if (savedEntry) {
       journalEntryElement.value = savedEntry;
     }
 
-    journalEntryElement.addEventListener("input", autosaveEntry);
+    journalEntryElement.addEventListener("input", () => {
+      entryModified = true;
+      autosaveEntry();
+    });
   } catch (error) {
     console.error("An error occurred while setting up the page:", error);
   }
 };
 
-// We use window.onload here instead of DOMContentLoaded to make sure that all external resources are loaded.
 window.onload = setupPage;
--- aiProcessing.js
+++ aiProcessing.js
@@ -20,37 +20,44 @@ const extractMainIssue = async (text) => {
     }
   };  
 
-const updateMainQuest = async (storage = window.localStorage) => {
-  try {
-    const journalEntryElement = document.getElementById("journal-entry");
-
-    const mainIssue = await extractMainIssue(journalEntryElement.value);
-
-    // Save the main issue in local storage
-    storage.setItem('mainQuest', mainIssue);
-
-    // Update main quest in the textarea
-    const mainQuestElement = document.getElementById('main-quest');
-    if (mainQuestElement) {
-      mainQuestElement.value = mainIssue;
+  const updateMainQuest = async (storage = window.localStorage) => {
+    try {
+      const lastUpdated = storage.getItem('lastUpdated');
+      const today = new Date().toISOString().split('T')[0]; // Get today's date in YYYY-MM-DD format
+  
+      // Check if the main quest has already been updated today
+      if (lastUpdated === today) return;
+  
+      const journalEntryElement = document.getElementById("journal-entry");
+      const mainIssue = await extractMainIssue(journalEntryElement.value);
+  
+      // Save the main issue in local storage
+      storage.setItem('mainQuest', mainIssue);
+      storage.setItem('lastUpdated', today); // Update the date when the main quest was last updated
+  
+      // Update main quest in the textarea
+      const mainQuestElement = document.getElementById('main-quest');
+      if (mainQuestElement) {
+        mainQuestElement.value = mainIssue;
+      }
+    } catch (error) {
+      console.error("An error occurred while updating the main quest:", error);
     }
-  } catch (error) {
-    console.error("An error occurred while updating the main quest:", error);
-  }
-};
-
-window.updateMainQuest = updateMainQuest;
-
-window.onload = function() {
-  // Update the streak as soon as the page loads
-  updateStreak();
-
-  // Load the main quest from local storage
-  const mainQuest = window.localStorage.getItem('mainQuest');
-  if (mainQuest) {
-    document.getElementById('main-quest').value = mainQuest;
-  }
-
-  // Update main quest after page load
-  updateMainQuest();
-};
+  };
+  
+  window.updateMainQuest = updateMainQuest;
+  
+  window.onload = function() {
+    // Update the streak as soon as the page loads
+    updateStreak();
+  
+    // Load the main quest from local storage
+    const mainQuest = window.localStorage.getItem('mainQuest');
+    if (mainQuest) {
+      document.getElementById('main-quest').value = mainQuest;
+    }
+  
+    // Update main quest after page load
+    updateMainQuest();
+  };
+  
